c4cede3a04adfe627b07aadcbc6b3a68a4d0aaca
pannainspiracja <pannainspiracja@37549c6c-b284-11dd-a561-c1dc4c822220>
2008-11-15 00:56:08 +0000

    dodana wersja solucji do 2008, referencje do xna, kontrolka do malowania

---
+++ trunk/OpenCascaseViewer/DrawingControl.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace OpenCascaseViewer
+{
+    public class DrawingControl : WinFormsGraphicsDevice.GraphicsDeviceControl
+    {
+        private ContentManager contentManager;
+
+        private VertexDeclaration vertexDeclaration, vertexDeclaration2;
+        private BasicEffect textureEffect, colorEffect;
+        private Texture2D glassTexture;
+
+        public Matrix View { get; set; }
+        public Matrix World { get; set; }
+        public Matrix Projection { get; set; }
+
+        private Vector3 camera = new Vector3(0, 300, 150);
+
+        private VertexPositionNormalTexture[] vertexData;
+        private int[] indexDataTriangles;
+
+
+        protected override void Initialize()
+        {
+            contentManager = new ContentManager(Services, "Content");
+
+            View = Matrix.CreateLookAt(camera, Vector3.Zero, Vector3.Up);
+            World = Matrix.Identity;
+            Projection = Matrix.CreatePerspectiveFieldOfView(1, GraphicsDevice.Viewport.AspectRatio, 1, 10000);
+
+            vertexDeclaration = new VertexDeclaration(GraphicsDevice, VertexPositionNormalTexture.VertexElements);
+
+            textureEffect = new BasicEffect(GraphicsDevice, null);
+
+            textureEffect.TextureEnabled = true;
+            textureEffect.Texture = glassTexture;
+
+            textureEffect.DirectionalLight0.Enabled = true;
+            textureEffect.DirectionalLight0.DiffuseColor = Vector3.One;
+            textureEffect.DirectionalLight0.Direction = Vector3.Normalize(new Vector3(1.0f, -1.0f, -1.0f));
+            textureEffect.DirectionalLight0.SpecularColor = Vector3.One;
+
+            textureEffect.DirectionalLight1.Enabled = true;
+            textureEffect.DirectionalLight1.DiffuseColor = new Vector3(0.5f, 0.5f, 0.5f);
+            textureEffect.DirectionalLight1.Direction = Vector3.Normalize(new Vector3(-1.0f, -1.0f, 1.0f));
+            textureEffect.DirectionalLight1.SpecularColor = new Vector3(0.5f, 0.5f, 0.5f);
+
+            glassTexture = contentManager.Load<Texture2D>("Glass");
+
+            vertexDeclaration2 = new VertexDeclaration(GraphicsDevice, VertexPositionColor.VertexElements);
+
+            colorEffect = new BasicEffect(GraphicsDevice, null);
+            colorEffect.VertexColorEnabled = true;
+            colorEffect.DiffuseColor = new Vector3(1.0f, 1.0f, 1.0f);
+
+            vertexData = new VertexPositionNormalTexture[3];
+            indexDataTriangles = new int[3];
+        }
+
+        protected override void Draw()
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            GraphicsDevice.VertexDeclaration = vertexDeclaration;
+            GraphicsDevice.RenderState.DepthBufferEnable = true;
+
+            GraphicsDevice.RenderState.CullMode = CullMode.CullCounterClockwiseFace;
+
+            textureEffect.World = World;
+            textureEffect.View = View;
+            textureEffect.Projection = Projection;
+
+            textureEffect.EnableDefaultLighting();
+
+            textureEffect.TextureEnabled = true;
+            textureEffect.Texture = glassTexture;
+
+            textureEffect.DirectionalLight0.Enabled = true;
+            textureEffect.DirectionalLight0.DiffuseColor = Vector3.One;
+            textureEffect.DirectionalLight0.Direction = Vector3.Normalize(new Vector3(1.0f, 1.0f, -1.0f));
+            textureEffect.DirectionalLight0.SpecularColor = Vector3.One;
+
+            textureEffect.Begin();
+
+            foreach (EffectPass pass in textureEffect.CurrentTechnique.Passes)
+            {
+                pass.Begin();
+
+                GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionNormalTexture>(PrimitiveType.TriangleList,
+                    vertexData, 0, vertexData.Length, indexDataTriangles, 0, indexDataTriangles.Length / 3);              
+
+                pass.End();
+            }
+
+            textureEffect.End();
+
+            colorEffect.World = World;
+            colorEffect.View = View;
+            colorEffect.Projection = Projection;
+
+            GraphicsDevice.VertexDeclaration = vertexDeclaration2;
+
+            colorEffect.Begin();
+
+            foreach (EffectPass pass in colorEffect.CurrentTechnique.Passes)
+            {
+                pass.Begin();
+
+      
+                pass.End();
+            }
+
+            colorEffect.End();
+        }
+       
+
+#region Mouse
+
+
+        float xAngle, yAngle;
+        MouseState lastMouseState;
+
+        protected override void OnMouseMove(System.Windows.Forms.MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            MouseState newState = Mouse.GetState();
+            if (newState.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+            {
+                int yDiff = lastMouseState.Y - newState.Y;
+                int xDiff = lastMouseState.X - newState.X;
+
+                xAngle += yDiff / 2;
+                yAngle -= xDiff / 2;
+
+                World =
+                    Matrix.CreateRotationZ(MathHelper.ToRadians((yAngle)))
+                    * Matrix.CreateRotationX(MathHelper.ToRadians(MathHelper.Clamp(xAngle, -90, 90)));
+
+                Invalidate();
+            }
+            lastMouseState = newState;
+        }
+
+#endregion
+
+    }
+}
--- trunk/OpenCascaseViewer/OpenCascadeViewer.Designer.cs
+++ trunk/OpenCascaseViewer/OpenCascadeViewer.Designer.cs
@@ -28,12 +28,84 @@ namespace OpenCascaseViewer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.drawingControl1 = new OpenCascaseViewer.DrawingControl();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(577, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 315);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(577, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // drawingControl1
+            // 
+            this.drawingControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawingControl1.Location = new System.Drawing.Point(0, 24);
+            this.drawingControl1.Name = "drawingControl1";
+            this.drawingControl1.Projection = new Microsoft.Xna.Framework.Matrix(0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F);
+            this.drawingControl1.Size = new System.Drawing.Size(577, 291);
+            this.drawingControl1.TabIndex = 2;
+            this.drawingControl1.Text = "drawingControl1";
+            this.drawingControl1.View = new Microsoft.Xna.Framework.Matrix(0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F);
+            this.drawingControl1.World = new Microsoft.Xna.Framework.Matrix(0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // OpenCascadeViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 337);
+            this.Controls.Add(this.drawingControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "OpenCascadeViewer";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private DrawingControl drawingControl1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
