cbe9f868e94ef8094d351a315abcdcf72cc6b77e
pannainspiracja <pannainspiracja@37549c6c-b284-11dd-a561-c1dc4c822220>
2008-11-20 23:53:13 +0000

    brakujacy plik

---
+++ trunk/OpenCascaseViewer/Code/Utility.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCascaseViewer
+{
+    public class Utility
+    {
+        public static void Triangulate(TopoDS.Shape topoShape, out gp.Pnt[] points, out gp.Pnt2d[] uvpoints, out int[] triangles)
+        {
+            TopExp.Explorer ex = new TopExp.Explorer(topoShape, TopAbs.ShapeEnum.FACE, TopAbs.ShapeEnum.SHAPE);
+
+            BRepMesh.General.Mesh(topoShape, 0.01);
+
+            points = null;
+            uvpoints = null;
+            triangles = null;
+
+            // Loop all faces
+            while (ex.More())
+            {
+
+                TopoDS.Face F = TopoDS.General.Face(ex.Current());
+                TopLoc.Location location = new TopLoc.Location();
+
+                Poly.Triangulation triangulation = BRep.Tool.Triangulation(F, location);
+
+                Poly.Array1OfTriangle arrayOfTriangle = triangulation.Triangles();
+
+                TColgp.Array1OfPnt p3d = triangulation.Nodes();
+
+                points = new gp.Pnt[p3d.Length()];
+                for (int i = 0; i < points.Length; ++i)
+                {
+                    points[i] = p3d.Value(i + 1);
+                }
+                if (location != null)
+                {
+                    gp.Trsf trsf = location.Transformation();
+                    for (int i = 0; i < points.Length; ++i)
+                    {
+                        trsf.Transforms(out points[i].x, out points[i].y, out points[i].z);
+                    }
+                }
+                TColgp.Array1OfPnt2d p2d = triangulation.UVNodes();
+                if (p2d != null)
+                {
+                    uvpoints = new gp.Pnt2d[p2d.Length()];
+                    for (int i = 0; i < uvpoints.Length; ++i)
+                    {
+                        uvpoints[i] = p2d.Value(i + 1);
+                    }
+                }
+                else
+                {
+                    uvpoints = null;
+                }
+                triangles = new int[arrayOfTriangle.Length() * 3];
+                int l = arrayOfTriangle.Length();
+                for (int i = 0; i < l; ++i)
+                {
+                    Poly.Triangle t = arrayOfTriangle.Value(i + 1);
+                    triangles[3 * i] = t.Value(1) - 1;
+                    triangles[3 * i + 1] = t.Value(2) - 1;
+                    triangles[3 * i + 2] = t.Value(3) - 1;
+                }
+
+                ex.Next();
+            }
+        }
+    }
+}
