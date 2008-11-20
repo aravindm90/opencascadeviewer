78d9075cb51e850d8e37c29fc5f6f27e67fdbe6d
tomasz.czekala <tomasz.czekala@37549c6c-b284-11dd-a561-c1dc4c822220>
2008-11-20 21:11:52 +0000

    Dodałem wczytywanie IGES, STEP, BREP. JEszcze powalczę z CSFDB

--- trunk/OpenCascaseViewer/OpenCascadeViewer.cs
+++ trunk/OpenCascaseViewer/OpenCascadeViewer.cs
@@ -9,6 +9,8 @@ namespace OpenCascaseViewer
 {
     public partial class OpenCascadeViewer : Form
     {
+        public List<TopoDS.Shape> viewedShapes = new List<TopoDS.Shape>();
+
         public OpenCascadeViewer()
         {
             InitializeComponent();
@@ -26,7 +28,60 @@ namespace OpenCascaseViewer
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                //load file
+                string[] parts = openFileDialog1.FileName.Split('.');
+                string type = parts[parts.Length - 1];
+                type = type.ToUpper();
+                switch (type)
+                {                    
+                    case "IGES":                 
+                        IGESControl.Reader igesReader = new IGESControl.Reader();
+                        if (igesReader.ReadFile(openFileDialog1.FileName) == IFSelect.ReturnStatus.RetDone)
+                        {
+                            igesReader.TransferRoots();
+                            viewedShapes.Clear();
+                            viewedShapes.Add(igesReader.OneShape());
+                        }
+                        else
+                            MessageBox.Show("Could not load file " + openFileDialog1.FileName);
+                        break;
+                    
+                    case "STEP":
+                        STEPControl.Reader stepReader = new STEPControl.Reader();
+                        if (stepReader.ReadFile(openFileDialog1.FileName) == IFSelect.ReturnStatus.RetDone)
+                        {
+                            viewedShapes.Clear();
+                            int numberOfRoots = stepReader.NbRootsForTransfer();
+                            for (int n = 1; n <= numberOfRoots; n++)
+                            {
+                                stepReader.TransferRoot(n);
+                                int numberOfShapes = stepReader.NbShapes();
+                                for (int i = 1; i <= numberOfShapes; i++)
+                                    viewedShapes.Add(stepReader.Shape(i));
+                            }                            
+                        }
+                        else
+                            MessageBox.Show("Could not load file " + openFileDialog1.FileName);
+                        break;
+
+                    //case "CSFDB":
+                        
+                    //    break;
+                    case "BREP":                        
+                        TopoDS.Shape shape = new TopoDS.Shape();
+                        BRep.Builder builder = new BRep.Builder(); ;
+                        if (BRepTools.General.Read(shape, openFileDialog1.FileName, builder))
+                        {
+                            viewedShapes.Clear();
+                            viewedShapes.Add(shape);
+                        }
+                        else
+                            MessageBox.Show("Could not load file " + openFileDialog1.FileName);                        
+                        break;
+                    default:
+                        MessageBox.Show("Unknown format \"" + type + "\"");
+                        break;
+                }                
+               
             }
         }
 
